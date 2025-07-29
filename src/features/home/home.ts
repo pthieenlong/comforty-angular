878804a61a0bba375bb84f9fd0243903cb21985c
pthieenlong <longpt.dev@gmail.com>
2025-07-30 00:05:06 +0700

    style: finish 90% home page

--- src/features/home/home.ts
+++ src/features/home/home.ts
@@ -1,12 +1,12 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './ui/home.html',
   styleUrls: ['./ui/home.css'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Home {
   banners = [
@@ -16,7 +16,7 @@ export class Home {
       image: 'assets/images/all-img/chair.png',
       discountImage: 'assets/images/all-img/discount.png',
       discountPercent: '15%',
-      discountText: 'Discount'
+      discountText: 'Discount',
     },
     {
       sub: 'Welcome to Chairy',
@@ -24,7 +24,7 @@ export class Home {
       image: 'assets/images/all-img/chair.png',
       discountImage: 'assets/images/all-img/discount.png',
       discountPercent: '15%',
-      discountText: 'Discount'
+      discountText: 'Discount',
     },
     {
       sub: 'Welcome to Chairy',
@@ -32,9 +32,94 @@ export class Home {
       image: 'assets/images/all-img/chair.png',
       discountImage: 'assets/images/all-img/discount.png',
       discountPercent: '15%',
-      discountText: 'Discount'
-    }
+      discountText: 'Discount',
+    },
   ];
-  
 
+  activeFilter = 'all';
+  products = [
+    {
+      id: 1,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-01.png',
+      category: 'featured',
+      isNew: true,
+    },
+    {
+      id: 2,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-02.png',
+      category: 'trending',
+      isNew: true,
+    },
+    {
+      id: 3,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-03.png',
+      category: 'featured',
+      isNew: true,
+    },
+    {
+      id: 4,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-04.png',
+      category: 'best-sellers',
+      isNew: true,
+    },
+    {
+      id: 5,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-04.png',
+      category: 'trending',
+      isNew: true,
+    },
+    {
+      id: 6,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-03.png',
+      category: 'trending',
+      isNew: true,
+    },
+    {
+      id: 7,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-02.png',
+      category: 'best-sellers',
+      isNew: true,
+    },
+    {
+      id: 8,
+      name: 'Library Stool Chair',
+      price: 20,
+      image: './assets/images/all-img/f-product-01.png',
+      category: 'newest',
+      isNew: true,
+    },
+  ];
+
+  get filteredProducts() {
+    if (this.activeFilter === 'all') {
+      return this.products;
+    }
+    return this.products.filter(
+      (product) => product.category === this.activeFilter
+    );
+  }
+
+  filterProducts(category: string) {
+    this.activeFilter = category;
+  }
+
+  isProductVisible(product: any): boolean {
+    return (
+      this.activeFilter === 'all' || product.category === this.activeFilter
+    );
+  }
 }
